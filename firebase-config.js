5e49aa5949e5cc8de45a598129b880b167370359
eclat3 <ninian0204@gmail.com>
2025-05-02 02:57:25 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,2 +1,2 @@
 // GASでデプロイした Webアプリの URL
-export const GAS_URL = 'https://script.google.com/macros/s/AKfycbzQSMJ3dD75eIBhT6_qbGXS0dU-ql_g77PhTEM8E4I2nbyNLBqYlx585YyHYbEFvxiP/exec';
+export const GAS_URL = 'https://script.google.com/macros/s/AKfycbzh0UGc1URxJdeYuZRh0eRASR8nTkJsdAJ4wnB3roj5IrewEcizfq-APqHt7_RZIe6a/exec';
